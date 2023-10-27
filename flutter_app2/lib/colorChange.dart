39e95478f8e893a1967a07921e94811c2c9f241d
Udval Enkhtaivan <74579865+udvale@users.noreply.github.com>
2023-10-26 22:33:38 -0400

    Delete flutter_app2 directory

--- flutter_app2/lib/colorChange.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class HexColor extends Color {
-  static int _getColorFromHex(String hexColor) {
-    return int.parse(hexColor, radix: 16);
-  }
-
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
-}
--- flutter_app2/lib/database/controller.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  static String apiUrl = 'http://127.0.0.1:8000';
-  static String endPoints = '/create/';
-}
